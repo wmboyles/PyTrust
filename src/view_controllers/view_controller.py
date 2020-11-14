3d5f37643b639c35da939311b63917bf1c849438
William Boyles <wmboyle2@ncsu.edu>
2020-11-14 01:24:15 -0500

    Removed/changed to only include things in min implementation
    The minimum implementation will focus mostly on things that I in 326
    dealt with. It is a proof of concept that implementing iTrust in Python
    is feasible without having too much of an unfinished product.
    
    If I have the time, I may go back and do the other parts that were also
    in iTrust, but I want to focus on the minimal implementation that
    surronds what we did.

--- src/view_controllers/view_controller.py
+++ src/view_controllers/view_controller.py
@@ -10,10 +10,10 @@ views.
 from .login import login_view_controller as login
 from .admin import admin_view_controller as admin
 #from .diseasecontrol import diseasecontrol_view_controller as diseasecontrol
-from .er import er_view_controller as er
+#from .er import er_view_controller as er
 #from .findexperts import findexperts_view_controller as findexperts
 from .hcp import hcp_view_controller as hcp
-from .labtech import labtech_view_controller as labtech
+#from .labtech import labtech_view_controller as labtech
 from .patient import patient_view_controller as patient
 #from .personnel import personnel_view_controller as personnel
 from .pharmacist import pharmacist_view_controller as pharmacist
@@ -26,10 +26,10 @@ blueprints = {
     login.login_view_controller,
     admin.admin_view_controller,
     #diseasecontrol.diseasecontrol_view_controller,
-    er.er_view_controller,
+    #er.er_view_controller,
     #findexperts.findexperts_view_controller,
     hcp.hcp_view_controller,
-    labtech.labtech_view_controller,
+    #labtech.labtech_view_controller,
     patient.patient_view_controller,
     #personnel.personnel_view_controller,
     pharmacist.pharmacist_view_controller,
