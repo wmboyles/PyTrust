68cbe36c42b9d9404362ad8aba0597f58ca58491
William Boyles <wmboyle2@ncsu.edu>
2020-11-14 17:41:27 -0500

    Add api bloodtype controller

--- src/api_controllers/api_controller.py
+++ src/api_controllers/api_controller.py
@@ -14,6 +14,7 @@ from .user.personnel import api_personnel_controller as personnel
 from .institution.pharmacy import api_pharmacy_controller as pharmacy
 from .institution.hospital import api_hospital_controller as hospital
 from .state import api_state_controller as state
+from .blood_type import api_blood_type_controller as blood_type
 
 # All api prefixes begin with /api
 BASE_URL = "/api"
@@ -27,4 +28,5 @@ blueprints = {
     pharmacy.api_pharmacy_controller,
     hospital.api_hospital_controller,
     state.api_state_controller,
+    blood_type.api_blood_type_controller,
 }
---
+++ src/api_controllers/blood_type/api_blood_type_controller.py
@@ -0,0 +1,29 @@
+"""
+This file contains API methods that relate to blood types.
+
+:author William Boyles:
+"""
+
+from flask import Blueprint, jsonify
+from http import HTTPStatus
+
+from ...persistent.blood_type.blood_type import BloodType
+
+api_blood_type_controller = Blueprint(
+    "api_blood_type_controlller",
+    __name__,
+    template_folder="templates",
+    static_folder="static",
+    url_prefix="/",
+)
+
+
+@api_blood_type_controller.route("/blood_types", methods=["GET"])
+def get_all_blood_types():
+    """
+    Gets a list of all blood types defined in the blood type enum
+    """
+
+    blood_types = [bt.value for bt in BloodType]
+
+    return jsonify(blood_types), HTTPStatus.OK
