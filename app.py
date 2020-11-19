f56cc8bdf6ab1a04d4c5c1ba530bd3711c3f4787
William Boyles <wmboyle2@ncsu.edu>
2020-11-18 19:10:59 -0500

    Update app and factory

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
+import os
+from dotenv import load_dotenv
+
 import src.factory as factory
+import src.models.persistent.data_generator as data_generator
 
 app = factory.create_app()
 
 if __name__ == "__main__":
+    load_dotenv()
+
+    app.secret_key = os.environ.get("SECRET_KEY")
+
+    app.config["DEBUG"] = os.environ.get("DEBUG") == "1"
+
+    db_user = os.environ.get("DB_USER")
+    db_pass = os.environ.get("DB_PASS")
+    db_name = os.environ.get("DB_NAME")
+    app.config[
+        "SQLALCHEMY_DATABASE_URI"
+    ] = f"mysql+pymysql://{db_user}:{db_pass}@localhost/{db_name}"
+
+    refresh_db = os.environ.get("REFRESH_DB") == "1"
+    if refresh_db:
+        data_generator.generate_sample_data()
+
     # app.run(ssl_context="adhoc")
     app.run()
--- src/factory.py
+++ src/factory.py
@@ -6,34 +6,19 @@ It must be named app.py for the flask run command to work.
 """
 
 from flask import Flask
-from dotenv import load_dotenv
-import os
 
 import src.models.persistent.persistent as persistent
 import src.view_controllers.view_controllers as view_controllers
 import src.api_controllers.api_controllers as api_controllers
-import src.models.persistent.data_generator as data_generator
 
 
-def create_app(refresh_db=False):
+def create_app():
     app = Flask(__name__)
 
-    # Get all our secrets as environment variables
-    load_dotenv()
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    app.secret_key = os.environ.get("SECRET_KEY")
-
-    app.config["DEBUG"] = os.environ.get("DEBUG") == "1"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = (
-        os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS") == "1"
-    )
-
-    db_user = os.environ.get("DB_USER")
-    db_pass = os.environ.get("DB_PASS")
-    db_name = os.environ.get("DB_NAME")
-    app.config[
-        "SQLALCHEMY_DATABASE_URI"
-    ] = f"mysql+pymysql://{db_user}:{db_pass}@localhost/{db_name}"
+    # will change. Here to suppress a warning
+    app.config["SQLALCHEMY_DATABASE_URI"] = ""
 
     persistent.db.init_app(app)
     persistent.ma.init_app(app)
@@ -45,8 +30,5 @@ def create_app(refresh_db=False):
         app.register_blueprint(api_blueprint, url_prefix=api_controllers.BASE_URL)
 
     app.app_context().push()
-
-    if refresh_db:
-        data_generator.generate_sample_data()
 
     return app
